daf21c7fa1be80d46eb34890db7cd4dd86ca9f9a
james <jnguy2018@gmail.com>
2022-03-16 03:35:53 -0700

    Set up template for game page (player-view)

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Authentication from './components/Authentication';
 import NotFound from './components/NotFound';
 import Login from './components/Login';
 import Register from './components/Register';
+import GameMain from './components/GameMain';
 
 const App = () => {
     return (
@@ -15,6 +16,7 @@ const App = () => {
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
             <Route path="*" element={<NotFound/>} />
+            <Route path="/game" element={<GameMain/>} />
           </Routes>
         </Router>
       </div>
