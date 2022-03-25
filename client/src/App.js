66ba4876b81d7c5fed4302f6628135efdc9aac75
james <jnguy2018@gmail.com>
2022-03-25 03:58:47 -0700

    Added protected routes and added React Context for 'global' state

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { useState } from 'react';
 
 import Authentication from './components/Authentication';
 import NotFound from './components/NotFound';
@@ -8,25 +9,33 @@ import Register from './components/Register';
 import GameMain from './components/GameMain';
 import HostDashboard from './components/HostDashboard'
 import UserJoin from './components/UserJoin'
-
+import Lobby from './components/Lobby';
 import SpotifyAPI from './components/SpotifyAPI';
+import RequireAuth from './components/RequireAuth';
+import Layout from './components/Layout';
 
 const App = () => {
+    const [userContext, setUserContext] = useState()
+
     return (
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Authentication/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="*" element={<NotFound/>} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Public routes */}
+          <Route path="/" element={<Authentication/>}/>
+        
+          {/* We want to protect these routes */}
+          <Route element={<RequireAuth/>}>
+            <Route path="/lobby" element={<Lobby/>} />
             <Route path="/game" element={<GameMain/>} />
             <Route path="/authenticate" element={<SpotifyAPI/>} />
-            <Route path="/user/host" element={<HostDashboard/>} />
+            <Route path="/user/host" element={<HostDashboard/>}/>
             <Route path="/user/join" element={<UserJoin/>} />
-          </Routes>
-        </Router>
-      </div>
+          </Route>
+
+          {/* Catch all pages */}
+          <Route path="*" element={<NotFound/>} />
+        </Route>
+      </Routes>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {AuthProvider} from './context/AuthProvider';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </AuthProvider>
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
