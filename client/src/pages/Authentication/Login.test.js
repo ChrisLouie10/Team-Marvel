c5a84699cfe8c1f57039cb2ebe4b7ef07114d868
james <jnguy2018@gmail.com>
2022-04-25 20:47:06 -0700

    Configuring test environment (1/x)

--- client/src/pages/Authentication/Login.test.js
+++ client/src/pages/Authentication/Login.test.js
@@ -1,10 +1,26 @@
-import { render } from "@testing-library/dom"
+import { experimentalStyled } from '@mui/material'
+import {render, screen} from '@testing-library/react'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import SignIn from './Login'
 
+// describe allows you to describe a whole test case for a component
 describe("Login Component", () => {
+  
+  // it keyword describes an event in this component (a test)
   it("rendered input", () => {
-    const { getByTestId } = render(<SignIn />);
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
+    )
+    // const { getByTestId } = render(
+    //     <SignIn />
+    // )
     const input = getByTestId("username")
-    expect(input).toBeTruthy();
+    expect(input).toBeTruthy()
   })
 })
