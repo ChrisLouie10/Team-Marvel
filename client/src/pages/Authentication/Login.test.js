42227104acbe84b1a15feb080b8a891f23349f00
James Nguyen <jnguy2018@gmail.com>
2022-04-26 01:37:49 -0700

    Configuring testing environment (3/3) ALL DONE WOOT

--- client/src/pages/Authentication/Login.test.js
+++ client/src/pages/Authentication/Login.test.js
@@ -1,63 +1,54 @@
-import { fireEvent, screen} from '@testing-library/react'
+import { fireEvent, render, screen} from '@testing-library/react'
 import React from 'react'
-import { unmountComponentAtNode } from 'react-dom'
-import { createRoot } from 'react-dom/client'
-import { act } from 'react-dom/test-utils'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {BrowserRouter as Router} from 'react-router-dom';
+import '@testing-library/jest-dom'
 
 import SignIn from './Login'
 
-let container = null
-
-beforeEach(() => {
-  console.log("Before each")
-  container = document.createElement("div")
-  document.body.appendChild(container)
-})
-
-afterEach(() => {
-  console.log("After each")
-  // unmountComponentAtNode(container)
-  // container.remove()
-  // container = null
-})
-
-// describe allows you to describe a whole test case for a component
+// describe allows you to describe a whole test suite for a component
 describe("<Login />", () => {
   
   // it keyword describes an event in this component (a test)
-  it("Should render username input", () => {
-    const root = createRoot(container)
-    act(() => {
-      root.render(<Router><SignIn /></Router>);
-    });
+  it("Should render helperText for a password length less than minimum", () => {
+    const { container, getByText, getByTestId } = render(<Router><SignIn /></Router>)
 
-    const usernameInput = document.querySelector("[data-testid=username]")
-    const passwordInput = document.querySelector("[data-testid=password]")
-    const submitBtn = document.querySelector("[data-testid=submit-btn]")
+    const username = getByTestId("username-textfield");
+    const password = getByTestId("password-textfield");
+    const submit = getByTestId("submit-btn");
 
-    act(() => {
+    fireEvent.change(username, { target: { value: "validusername" } });
+    fireEvent.change(password, { target: { value: "evil" } });
+    fireEvent.click(submit);
 
-      // enter credentials
-      usernameInput.value = "j3"
-      passwordInput.value = "validpassword"
+    expect(getByText('"Password" length must be at least 6 characters long')).toBeInTheDocument()
+  })
 
-      submitBtn.dispatchEvent(
-        new MouseEvent("click", {bubbles:true})
-      )
-    })
+  it("Should render helperText for an empty password", () => {
+    const { container, getByText, getByTestId } = render(<Router><SignIn /></Router>)
 
-    
-    const errors = document.querySelector("[data-testid=errors]")
-    console.log(errors.textContent)
-    // console.log(usernameInput.value)
-    // console.log(errors)
-    // act(() => {
-    //   console.log(submitBtn)
-    //   submitBtn.dispatchEvent(
-    //     new MouseEvent("click", {bubbles:true})
-    //   )
-    // })
+    const username = getByTestId("username-textfield");
+    const password = getByTestId("password-textfield");
+    const submit = getByTestId("submit-btn");
+
+    fireEvent.change(username, { target: { value: "validusername" } });
+    fireEvent.change(password, { target: { value: "" } });
+    fireEvent.click(submit);
+
+    expect(getByText('"Password" is not allowed to be empty')).toBeInTheDocument()
+  })
+
+  it("Should render helperText for a password length more than the maximum", () => {
+    const { container, getByText, getByTestId } = render(<Router><SignIn /></Router>)
+
+    const username = getByTestId("username-textfield");
+    const password = getByTestId("password-textfield");
+    const submit = getByTestId("submit-btn");
+
+    fireEvent.change(username, { target: { value: "validusername" } });
+    fireEvent.change(password, 
+      { target: { value: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" } });
+    fireEvent.click(submit);
+
+    expect(getByText('"Password" length must be less than or equal to 64 characters long')).toBeInTheDocument()
   })
 })
