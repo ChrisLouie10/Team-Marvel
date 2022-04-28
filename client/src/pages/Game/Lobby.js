e1f0b90e1914567e1285acdc34771e69252b9be8
James Nguyen <jnguy2018@gmail.com>
2022-04-27 23:52:49 -0700

    Implemented lobby UI and button logic for leaving the lobby

--- client/src/pages/Game/Lobby.js
+++ client/src/pages/Game/Lobby.js
@@ -4,14 +4,34 @@ import socket from '../../components/Socket'
 import { useNavigate, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const PlayerLabel = styled('div')(({ theme }) => ({
+  ...theme.typography.button,
+  backgroundColor: 'hsla(173, 44%, 56%, 1)',
+  padding: '.5rem',
+}));
+
+// todo: display playlist name on screen
+// todo: create a box that lists all players in lobby
+// todo: create a button that navigates players back to homepage
+// todo: display username on top right
+// todo: show begin button for host only
+
 const Lobby = () => {
   const { data } = useAuth();
   const { state } = useLocation();
   const navigate = useNavigate();
   const [players, setPlayers] = useState(state.players)
+  const [playlist, setPlaylist] = useState({
+    playlist: state.playlist, 
+    playlistName: state.playlistName
+  })
 
-  console.log(data)
-  console.log(state)
   useEffect(() => {
     socket.off()
 
@@ -34,23 +54,119 @@ const Lobby = () => {
     })
   })
   
-  const handleClick = (e) => {
+  const startGame = (e) => {
     e.preventDefault()
     socket.emit('startGame', state)
   }
 
+  const backToHome = (e) => {
+    e.preventDefault()
+    socket.emit('playerLeftGame')
+    navigate('/user/host', {replace: true})
+  }
+
   return (
-    <div>
-      Lobby {state.gamePin}
-      {players.map((player) => (
-        player.playerSocketId == state.hostSocketId ?
-          <li key={player.playerId}>This is host: {player.playerName}</li> :
-          player.playerId == data.id ?
-            <li key={player.playerId}>This is me: {player.playerName}</li> :
-            <li key={player.playerId}>{player.playerName}</li>
-      ))}
-      {socket.id == state.hostSocketId && <button onClick={handleClick}>Start Game</button>}
-    </div>
+    <Box
+      sx={{
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        minHeight: '100vh',
+        minWidth: '100vw',
+        backgroundColor: 'hsla(0, 0%, 100%, 1)'
+      }}>
+      <Box sx={{
+        display: 'flex',
+        position: 'static',
+        width: '100%',
+      }}>
+        <Box sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          position: 'relative',
+          width: '100%',
+          mt: '1rem',
+          mr: '1rem',
+        }}>
+          <Button variant="outlined" onClick={backToHome} sx={{
+            position: 'relative',
+            backgroundColor: 'hsla(0, 0%, 13%, 1)',
+            color: 'hsla(0, 0%, 100%, 1)',
+            '&:hover': {
+              backgroundColor: 'hsla(0, 0%, 13%, .75)',
+              borderColor: 'hsla(0, 0%, 13%, .75)',
+              boxShadow: 'none',
+            },
+            }}>
+              Back to Home
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{
+        position: 'relative',
+        width: '70%',
+        minHeight: '60vh',
+        mt: '5rem',
+      }}>
+        <Typography variant="h2" sx={{ fontWeight: '900', mb: '1rem', }}>Playlist: {playlist.playlistName}</Typography>
+        <Typography variant="h5">Lobby: {state.gamePin}</Typography>
+        <Grid
+          sx={{
+            position: 'relative',
+            display: 'flex',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+            gap: '2rem 3rem',
+            padding: '2.5rem',
+          }}>
+          {players.map((player) => (
+            // if you're the player and the host
+            player.playerSocketId == state.hostSocketId && player.playerId == data.id ?
+              <PlayerLabel key={player.playerId} sx={{ backgroundColor: 'hsla(57, 96%, 45%, 1)' }}>
+                HOST {player.playerName}</PlayerLabel>  :
+            // if you're the player and not the host
+            player.playerId == data.playerId ?
+              <PlayerLabel key={player.playerId} sx={{ backgroundColor: 'hsla(57, 96%, 45%, 1)' }}>
+                {player.playerName}
+              </PlayerLabel> :
+            // if you're not the player and not the host
+            player.playerSocketId == state.hostSocketId ?
+              <PlayerLabel key={player.playerId}>HOST {player.playerName}</PlayerLabel> :
+              <PlayerLabel key={player.playerId}>{player.playerName}</PlayerLabel>
+          ))}
+        </Grid>
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        mb: '4rem',
+        width: '100%',
+      }}>
+        {socket.id == state.hostSocketId 
+          && 
+        <Button 
+          variant="outlined" 
+          onClick={startGame}
+          sx={{
+            py: '1rem',
+            borderRadius: '50px',
+            backgroundColor: 'hsla(0, 0%, 13%, 1)',
+            color: 'hsla(0, 0%, 100%, 1)',
+            '&:hover': {
+              backgroundColor: 'hsla(0, 0%, 13%, .75)',
+              borderColor: 'hsla(0, 0%, 13%, .75)',
+              boxShadow: 'none',
+            },
+            width: '15rem',
+          }}>
+            Begin
+        </Button>}
+      </Box>
+    </Box>
   )
 }
 
