899506b1176a032fab0c38fddcc8029fceaa840b
ChrisLouie10 <christopherlouie16@gmail.com>
2022-05-06 19:26:13 -0700

    changed games/player/:id to show entire game data

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { findAllGames, findGameById, findGamesByPlayerId } = require('../db/dao/gameDao');
-const { gameToObject, gameToPlayerScore } = require('../lib/converters/gameConverter');
+const { gameToObject } = require('../lib/converters/gameConverter');
 
 router.get('/', async (req, res) => {
   const games = await findAllGames()
@@ -16,7 +16,7 @@ router.get('/:id', async (req, res) => {
 router.get('/player/:id', async (req, res) => {
   const games = await findGamesByPlayerId(req.params.id);
   if(!games) return res.status(404).json({message: 'Games not found'});
-  return res.status(200).json(games.map((game) => gameToPlayerScore(game, req.params.id)));
+  return res.status(200).json(games.map((game) => gameToObject(game, req.params.id)));
 })
 
 module.exports = router;
